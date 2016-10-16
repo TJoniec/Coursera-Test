14942062a0b0c4685c82b28a8fc1b9a23050b7ca
TJoniec <tony.joniec@gmail.com>
2016-10-15 23:25:58 -0400

    Added function capability example

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,11 +1,17 @@
 (function () {
 'use strict';
 
-
 angular.module('myFirstApp', [])
 
 .controller('MyFirstController', function($scope) {
 $scope.name="Tony in Div 1";
+
+
+
+$scope.functionReturn = (function ($scope){
+    return "Hello from a function call";
+})
+
 })
 
 .controller('MySecondController', function($scope) {
